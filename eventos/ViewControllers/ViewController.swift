4c297d13bb899d87915faa0eaf8b38917fc06399
MOSS17 <42181277+MOSS17@users.noreply.github.com>
2020-11-09 20:54:45 -0700

    first commit

---
+++ eventos/ViewControllers/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  eventos
+//
+//  Created by Alumno on 11/9/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var eventos: [Evento] = []
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 284
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celdaEvento = tableView.dequeueReusableCell(withIdentifier: "celdaEvento") as! CeldaEventoController
+        let evento = eventos[indexPath.row]
+        
+        celdaEvento.lblTitulo.text = evento.titulo
+        celdaEvento.lblLugar.text = evento.lugar
+        celdaEvento.lblFecha.text = evento.fecha
+        celdaEvento.lblHora.text = evento.hora
+        
+        celdaEvento.imgEvento.image = UIImage(named: evento.imagen)
+        
+        return celdaEvento
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Eventos"
+        
+        eventos.append(Evento(titulo: "Baile de la Brissa", fecha: "20 de noviembre", hora: "8 pm", lugar: "Explanada del TOG", imagen: "brissa"))
+        eventos.append(Evento(titulo: "Concierto del laberinto", fecha: "25 de diciembre", hora: "9 pm", lugar: "Explanada del TOG", imagen: "laberinto"))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
